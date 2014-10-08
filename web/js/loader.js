c14fb468edfe4677def1820870592750aeaec24c
mortinho <mortoboladu@gmail.com>
2014-10-08 08:42:51 -0300

    merge test

--- web/js/loader.js
+++ web/js/loader.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//merge test
 // parser de parametros de url, null se nao existir
 
 function getDashboardMenu(projectList) {
